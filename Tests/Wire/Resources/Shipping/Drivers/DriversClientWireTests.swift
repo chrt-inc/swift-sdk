edb6de667a46b5c92b53aeb09c9bb9ba0bf256d5
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2026-05-28 07:53:31 +0000

    Release 1.710.0

--- Tests/Wire/Resources/Shipping/Drivers/DriversClientWireTests.swift
+++ Tests/Wire/Resources/Shipping/Drivers/DriversClientWireTests.swift
@@ -301,9 +301,6 @@ import Chrt
         )
         let response = try await client.shipping.drivers.listOrgMembersAndDriversV1(
             search: "search",
-            filterRole: [
-                .owner
-            ],
             filterAvailableAccordingToDriver: true,
             filterAvailableAccordingToOperators: true,
             sortBy: .firstName,
